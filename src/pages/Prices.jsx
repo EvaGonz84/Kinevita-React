8834b6e6ee2ea7434d3576cea0f01ccb7314e137
Eva González <evagonzalez.tic@gmail.com>
2023-06-01 17:30:36 +0200

    content added to prices page

--- src/pages/Prices.jsx
+++ src/pages/Prices.jsx
@@ -1,5 +1,45 @@
+import styled from "styled-components";
+
+const Container = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 1rem;
+  box-sizing: border-box;
+  width: 100%;
+  background-color: #fbf9f9;
+
+  h2 {
+    text-transform: uppercase;
+    font-size: 1.75rem;
+  }
+
+  p {
+    font-size: 1.25rem;
+  }
+`;
+
+
 const Prices = () => {
-  return "Prices";
+  return (
+    <>
+      <Container>
+       
+          <h2>Fisioterapia Kinevita</h2>
+          <h3>Precios fisioterapia en Barcelona y alrededores</h3>
+        
+        <p>
+          Fisioterapia y rehabilitación a domicilio, drenaje linfático manual.
+          Sesiones privadas de 50 minutos.
+        </p>
+
+        <p>
+          Sesiones de pilates, gimnasia abdominal hipopresiva, gimnasia para
+          mayores. Sesiones y clases privadas de 50 minutos.
+        </p>
+      </Container>
+    </>
+  );
 };
 
 export { Prices };
